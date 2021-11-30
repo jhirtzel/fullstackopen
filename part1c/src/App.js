3db68b650e62c0d25a2c6ef1537cc8094d2eddcf
jhirtzel <84430315+jhirtzel@users.noreply.github.com>
2021-11-30 00:23:29 -0500

    pre-exercise 1.4, add notes on component syntax

--- part1c/src/App.js
+++ part1c/src/App.js
@@ -1,25 +1,58 @@
 import React, {useState} from 'react';
 
-const Display = ({counter}) => <div>{counter}</div>
-
+/* Components */
+// this component uses () syntax because it has a single statement (the <button> element)
+// and the return() call is implied
 const Button = ({clickHandler, label}) => (
   <button onClick={clickHandler} >{label}</button>
 )
 
-const App = () => {
-  // console.log("App execute...")
-  const [counter, setCounter] = useState(0)
-  const incrementCounter = () => {setCounter(counter + 1)}
-  const decrementCounter = () => {setCounter(counter - 1)}
-  const resetCounter = () => {setCounter(0)}
+// this component uses the {} syntax because it has multiple statements
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return(
+      <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit</div>
+    )
+  } else {
+    return(
+      <div>
+        debugger
+        {props.allClicks.map(elem => <p>{elem}</p>)}
+      </div>
+    )
+  }
+}
 
-  // console.log("rendering..." + counter)
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAllClicks] = useState([])
+  
+  /* Event Handlers */
+  const handleLeftClick = () => {
+    setAllClicks(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAllClicks(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
+  const handleReset = () => {
+    setAllClicks(allClicks.concat('Reset'))
+    setLeft(0)
+    setRight(0)
+  }
+
   return (
     <>
-      <Display counter={counter} />
-      <Button label="Increment" clickHandler={incrementCounter} />
-      <Button label="Reset" clickHandler={resetCounter} />
-      <Button label="Decrement" clickHandler={decrementCounter} />
+      {left}
+      <Button label="Left" clickHandler={handleLeftClick}/>
+      <Button label="Reset" clickHandler={handleReset} />
+      <Button label="Right" clickHandler={handleRightClick}/>
+      {right}
+      <History allClicks={allClicks} />
     </>
   )
 }

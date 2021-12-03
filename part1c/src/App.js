7dfdb74a8df7c8e78c5fbfb05e2563c65469db9e
jhirtzel <84430315+jhirtzel@users.noreply.github.com>
2021-12-02 19:13:18 -0500

    exercise 1.7

--- part1c/src/App.js
+++ part1c/src/App.js
@@ -8,30 +8,20 @@ const Button = ({clickHandler, label}) => (
 )
 
 const Stats = ({posCount, neuCount, negCount}) => {
+  const sum = posCount + neuCount + negCount;
+  let avg = 0;
+  if (sum !== 0) avg = (posCount/(posCount+negCount))*100;     
+
   return(
     <>
     <h1>Statistics</h1>
     <p>Positive: {posCount}</p>
     <p>Neutral: {neuCount}</p>
     <p>Negative: {negCount}</p>
+    <p>Total votes: {sum}</p>
+    <p>Average: {avg}% positive</p>
     </>
   )
-}
-
-// this component uses the {} syntax because it has multiple statements
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return(
-      <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit</div>
-    )
-  } else {
-    return(
-      <div>
-        debugger
-        {props.allClicks.map(elem => <p>{elem}</p>)}
-      </div>
-    )
-  }
 }
 
 const App = () => {

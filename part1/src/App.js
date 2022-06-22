a1246efb098bb05d4623ed23236593a50a4ef4d9
jhirtzel <84430315+jhirtzel@users.noreply.github.com>
2022-06-22 11:11:11 -0400

    exercise 1.14

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,82 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react'
 
-const Header = (props) => {
-  console.log("HEADER COMPONENT")
-  return(
-    <div>
-      <h1>{props.course.name}</h1>
-    </div>
-  )
-}
-
-const Footer = (props) => {
-  console.log("FOOTER COMPONENT")
-  return(null)
-}
-
-const Content = (props) => {
-  console.log("CONTENT COMPONENT")
-  // return a Part component for each entry in the props
-  const listOfParts = props.course.parts.map( part => {
-    return (
-      <Part key={part.name} name={part.name} exercises={part.exercises} />
-    )}
-  )
-  return([listOfParts])
-}
-
-const Part = (props) => {
-  console.log("PART COMPONENT")
-  console.log(props)
-  return (
-    <div id="Part">
-      <p>{props.name} {props.exercises}</p>
-    </div>
-  )
-}
-
-const Total = (props) => {
-  console.log("TOTAL COMPONENT")
-  let sumOfExercises = 0
-  props.course.parts.forEach( element => {
-    sumOfExercises += element.exercises
-  })
-
-  return(
-    <div>
-      <p>Number of exercises = {sumOfExercises}</p>
-    </div>
-  )
-}
+const Button = ({clickHandler, label}) => (
+  <button onClick={clickHandler} >{label}</button>
+)
 
 const App = () => {
-  console.log("APP COMPONENT")
-  const course = {
-    "name": "Half Stack application development",
-    "parts": [
-      {
-        "name": 'Fundamentals of React',
-        "exercises": 10
-      },
-      {
-        "name": 'Using props to pass data',
-        "exercises": 7
-      },
-      {
-        "name": 'State of a component',
-        "exercises": 14
-      }
-    ]
-  }
-    
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
+  
+  const [selected, setSelected] = useState(0)
+	const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+
+	// state handler to choose a random anecdote element
+	function handleNext() {
+		setSelected(getRandBetween(0, anecdotes.length));
+	}
+
+	// state handler to increment the votes at a given index
+	function handleVote() {
+		let updVotes = [...votes];
+		updVotes[selected] += 1;
+		setVotes(updVotes);
+	}
+
+  // return a range exclusive of the max value, to be used in array indexing
+	function getRandBetween(min, max) {
+		min = Math.ceil(min);
+		max = Math.floor(max);
+		return Math.floor(Math.random() * (max - min) + min);
+	}
+
+	function getTopVote() {
+		console.table(votes)
+		let topVote = 0;
+		votes.forEach((v) => {
+			if (v > topVote) topVote = v;
+		})
+		return votes.indexOf(topVote);
+	}
+
   return (
     <div>
-      <Header course={course}/>
-      <Content course={course}/>
-      <Total course={course}/>
-      <Footer />
+      <p>{anecdotes[selected]}</p>
+			<p>{votes[selected]} votes</p>
+			<Button label="next anecdote" clickHandler={handleNext}/>
+			<Button label="Vote for this anecdote" clickHandler={handleVote}/>
+			<h1>Top Anecdote is</h1> 
+			<p>{anecdotes[getTopVote()]}</p>
     </div>
   )
 }
 
-export default App;
+export default App

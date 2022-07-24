f99bd8c47124061da2e4b6b9f2afa64b1fef418f
jhirtzel <84430315+jhirtzel@users.noreply.github.com>
2022-07-24 14:44:21 -0400

    part2 exercise 2.3

--- part2/src/components/Course.js
+++ part2/src/components/Course.js
@@ -1,7 +1,7 @@
 import CoursePart from './CoursePart'
 
 const Course = ({course}) => {
-	console.log("Course>>",course)
+	// console.log(course.parts)
 	return (
 		<div>
 			<h2>{course.name}</h2>
@@ -11,8 +11,15 @@ const Course = ({course}) => {
 					<CoursePart key={part.id} part={part}/>
 				)}
 			</ul>
+			<h3>Total of {getTotalExercises(course.parts)} exercises</h3>
 		</div>
 	)
+
+	function getTotalExercises(parts) {
+		parts.map(e => console.log(e.exercises))
+		
+		return parts.reduce( (total, next) => total + +next.exercises, 0 ) 
+	}
 }
 
 export default Course
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
 run the app with `npm start`
 
-[Left off here](https://fullstackopen.com/en/part1/a_more_complex_state_debugging_react_apps) at exercise 1.12
+[Left off here](https://fullstackopen.com/en/part2/rendering_a_collection_modules)
